a361131fc93fb6cad195b5c0b62608c033f477a3
Satheesh Gajula <44570005+satheesh22g@users.noreply.github.com>
2021-06-14 12:41:55 +0530

    Create mp3cutter.py

---
+++ mp3cutter.py
@@ -0,0 +1,21 @@
+​from pydub import AudioSegment
+
+files_path = ''
+file_name = ''
+
+startMin = 9
+startSec = 50
+
+endMin = 13
+endSec = 30
+
+# Time to miliseconds
+startTime = startMin*60*1000+startSec*1000
+endTime = endMin*60*1000+endSec*1000
+
+# Opening file and extracting segment
+song = AudioSegment.from_mp3( files_path+file_name+'.mp3' )
+extract = song[startTime:endTime]
+
+# Saving
+extract.export( file_name+'-extract.mp3', format="mp3")
